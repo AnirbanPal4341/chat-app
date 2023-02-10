d9425194d57ab6bebc031740db8872a49bd0144a
Anirban Pal <63857297+AnirbanPal4341@users.noreply.github.com>
2023-02-10 16:34:30 +0530

    Add files via upload

--- auth/user-db/user.js
+++ auth/user-db/user.js
@@ -1,1 +1,23 @@
+let users = [
+    {
+      id: 1,
+      username: 'bob',
+      password: '123',
+    },
+    {
+      id: 2,
+      username: 'john',
+      password: '456',
+    },
+    {
+      id: 3,
+      username: 'max',
+      password: '789',
+    },
+  ];
 
+async function findOne(username) {
+  return users.find(user => user.username === username);
+}
+
+module.exports = {findOne} ;
