7d8c48b8e7aabe3215163febb6e1ec523e99d0dc
Anirban Pal <63857297+AnirbanPal4341@users.noreply.github.com>
2023-02-10 16:32:54 +0530

    Add files via upload

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const authService = require('../auth/auth-service');
 
+router.post('/', async (req, res) => {
+    let { username, password } = req.body;
+    const user = await authService.validateUser(username,password);
+    if(user){
+        res.status(201).json(user);
+    }
+    else {
+        res.status(400).json({ msg: `Auth Failed!` });
+      }
+    });
+
+module.exports = router;
